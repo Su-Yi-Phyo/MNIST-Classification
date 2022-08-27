b5d1c128f4c011e02ad6998a9da7f8d7a9077369
Su Yi Phyo <84232838+Su-Yi-Phyo@users.noreply.github.com>
2022-08-27 13:53:38 +0630

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,6 +6,16 @@ import pickle
 
 st.set_page_config(
      page_title="Digit Recognizer")
+st.markdown(
+  f"""
+    <style>
+     .stApp {{
+        background-image: url("https://img.freepik.com/premium-photo/background-diabetic-disease-concept-with-copy-space-world-diabetes-day-banner_132254-879.jpg?w=2000");
+        background-attachment: fixed;
+        background-size: cover}}
+     </style>
+  """,
+unsafe_allow_html=True)
 
 st.header('Digit Recognizer')
 uploadfile = st.file_uploader("Upload the digit image you want to test:")
